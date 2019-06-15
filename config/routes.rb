95db840c1a329da63a87359214ae42648fac398e
Riley Mathews <rileymathews@Rileys-MacBook-Pro.local>
2019-06-15 13:31:35 -0500

    created app controller and view to get further in feature spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'index#index'
+  get '/app', to: 'app#index'
 end
