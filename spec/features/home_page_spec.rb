adcad77241739984d11f9573275a97139fecaa5c
Riley Mathews <rileymathews@Rileys-MacBook-Pro.local>
2019-06-15 13:13:23 -0500

    wrote feature spec for expected user flow

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -9,9 +9,13 @@ RSpec.feature 'home page', type: :feature do
     expect(page).to have_text('show index')
   end
 
-  scenario 'user can see a link to the app' do
+  scenario 'user sees link to app, clicks it, and is redirected to login page' do
     visit '/'
 
     expect(page).to have_selector(:css, 'a[href="/app"]')
+
+    click_on 'Goto App'
+
+    expect(page).to have_content('Login Required')
   end
 end
