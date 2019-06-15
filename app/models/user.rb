33f3369ecc4f8a3980c1731d5daa37caea8daf79
Riley Mathews <rileymathews@Rileys-MacBook-Pro.local>
2019-06-15 11:02:58 -0500

    added validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :username, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a username' do
+    actual = User.new(username: '')
+
+    expect(actual.valid?).to be(false)
+  end
+
+  it 'is valid with a username' do
+    actual = User.new(username: 'username')
+
+    expect(actual.valid?).to be(true)
+  end
 end
