6a4e8c61b5d89b739da523dc10219fc535cf63e4
Riley Mathews <rileymathews@Rileys-MacBook-Pro.local>
2019-06-15 13:07:49 -0500

    added link to app page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,17 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.feature "home page", type: :feature do
-  scenario "user visits the homepage" do
+require 'rails_helper'
+
+RSpec.feature 'home page', type: :feature do
+  scenario 'user visits the homepage' do
     visit '/'
 
-    expect(page).to have_text("show index")
+    expect(page).to have_text('show index')
+  end
+
+  scenario 'user can see a link to the app' do
+    visit '/'
+
+    expect(page).to have_selector(:css, 'a[href="/app"]')
   end
 end
